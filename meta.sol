4d12210b073a68a81cacae2a53fab9cc5d2a486d
akki75 <74503361+akki75@users.noreply.github.com>
2023-11-04 14:42:04 +0530

    this is code

---
+++ meta.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract MyToken {
+
+    // public variables here
+    string public Token_Name = "Akshay";
+    string public Token_Abbr= "Anand";
+    uint public total_supply = 0;
+
+    // mapping variable here
+    mapping(address => uint) public balance;
+
+    // mint function
+    function mint(address _addr, uint _value) public {
+        total_supply += _value;
+        balance[_addr] += _value;
+    }
+
+    // burn function
+    function burn(address _addr, uint _value) public {
+        if(balance[_addr] >= _value) {
+            total_supply -= _value;
+            balance[_addr] -= _value;
+    }
+    }
+}
